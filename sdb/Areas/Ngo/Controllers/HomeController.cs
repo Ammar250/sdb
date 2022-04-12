d9ff4f8eb6da589f780dd122cbe326533908ca99
muhammad ammar <ammarghaffar7@gmail.com>
2022-04-13 03:59:00 +0500

    commit code

--- sdb/Areas/Ngo/Controllers/HomeController.cs
+++ sdb/Areas/Ngo/Controllers/HomeController.cs
@@ -3,15 +3,41 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using sdb.Models;
+using sdb.Repository;
+using sdb.Data;
 
 namespace sdb.Areas.Ngo.Controllers
 {
     [Area("Ngo")]
     public class HomeController : Controller
     {
+
+        private readonly SdbDBContext _db;
+        public HomeController(SdbDBContext db)
+        {
+            _db = db;
+        }
+
+
         public IActionResult Index()
+        {
+            var displaydata = _db.SdbSystemUsers.ToList();
+            return View(displaydata);
+        }
+
+        [HttpGet]
+        public IActionResult AddOrEdit(int id =0)
+        {
+
+            return View(new SdbSystemUsers());
+
+        }
+        [HttpPost]
+        public IActionResult AddorEdit ()
         {
             return View();
         }
     }
+        
 }
